1711348806a90bbecddfc04ef535ba38928990d4
isma-91 <isma91@hotmail.it>
2023-02-06 14:24:11 +0100

    import vueRouter, create vueRoutes & Pages(files)

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,23 +1,6 @@
 <template>
     <div>
-        <div class="container">
-            <h1  class="text-center m-4">Post FrontOffice</h1>
-            <ul>
-                <!-- <li v-for="post in arrPosts" :key="post.id">{{ post.title }}</li> -->
-                <div class="row g-3">
-                    <div v-for="post in arrPosts" :key="post.id" class="col-sm-6 col-md-4">
-                        <div class="card h-100">
-                            <img :src="post.image" :alt="post.title">
-                            <div class="card-body d-flex flex-column">
-                                <h4 class="card-title">{{ post.title }}</h4>
-                                <p class="card-text flex-grow-1">{{ post.excerpt }}</p>
-                                <a :href="'/posts/' + post.slug" class="btn btn-primary"> Leggi</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </ul>
-        </div>
+
     </div>
 </template>
 
